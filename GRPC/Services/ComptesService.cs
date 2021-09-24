b2f912c95faea82dbf80fc222151057b2539ced2
Gabriel Daoust <gabrieldaoust1@hotmail.com>
2021-09-24 11:56:52 -0400

    Définition des protosImplémentation des objets

---
+++ GRPC/Services/ComptesService.cs
@@ -0,0 +1,16 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace GRPC
+{
+    public class ComptesService : Compte.CompteBase
+    {
+        private readonly ILogger<ComptesService> _logger;
+        public ComptesService(ILogger<ComptesService> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
