b9c42d21dfb489ed6ec11f3644e170b2327e15fc
Gabriel Daoust <gabrieldaoust1@hotmail.com>
2021-09-24 11:56:52 -0400

    Implémentation des objets

---
+++ GRPC/Services/ComptesService.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace GRPC
+{
+    public class ComptesService : Compte.CompteBase
+    {
+        private readonly ILogger<ComptesService> _logger;
+        public ComptesService(ILogger<ComptesService> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
---
+++ GRPC/Services/TransactionsService.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace GRPC
+{
+    public class TransactionsService : Transaction.TransactionBase
+    {
+        private readonly ILogger<TransactionsService> _logger;
+        public TransactionsService(ILogger<TransactionsService> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
