dc9dbfa38f9bac5df7956220dca8525a45ef50ce
Gabriel Daoust <gabrieldaoust1@hotmail.com>
2021-10-08 21:01:00 -0400

    Moq CreateAccount

--- Tests/TestAccountsController.cs
+++ Tests/TestAccountsController.cs
@@ -1,5 +1,9 @@
+using DataAccessLayer.Repositories;
+using Domain.Interfaces;
 using Domain.Models;
+using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
+using Moq;
 using REST.Controllers;
 using System;
 using System.Threading.Tasks;
@@ -9,11 +13,28 @@ namespace Tests
 {
     public class TestAccountsController
     {
-        private AccountsController _testAccountsController = null;
+
         [Fact]
-        public void TestAccountCreate()
+        public async Task FindAccount_ActionResult_ReturnsNotFoundResultForNonExistentAccounts()
         {
-            AccountModel a = new AccountModel
+            //Arrange
+            Mock<IAccountsRepository> mockAccountRepository = new();
+            AccountsController controller = new(mockAccountRepository.Object);
+            int nonExistentAccountId = 999;
+
+            //Act
+            var response = await controller.AccountFind(nonExistentAccountId);
+
+            //Assert
+            Assert.NotNull(response);
+            Assert.IsType<ActionResult<AccountModel>>(response);
+            Assert.IsType<NotFoundResult>(response.Result);
+        }
+        [Fact]
+        public void CreateAccount_ActionResult_ValidResultForExistentAccounts()
+        {
+            //Arrange
+            AccountModel a = new()
             {
                 AccountNumber = 1,
                 AccountBalance = 99,
@@ -22,20 +43,31 @@ namespace Tests
                 AccountHolderLastName = "Houde",
                 IsActive = true
             };
-            /*{
-                "accountNumber": 1,
-                "accountBalance": 99,
-                "accountCreationDate": "1999-10-01T00:00:00",
-                "isActive": true
-            }*/
+            Mock<IAccountsRepository> mockAccountRepository = new();
+            mockAccountRepository.Setup(m => m.AccountCreate(a)).ReturnsAsync(a);
 
-
-
-
-            var actual = _testAccountsController.AccountCreate(a);
+            //Act
+            var response = mockAccountRepository.Object.AccountCreate(a).Result;
 
             //Assert
-            //Assert.Equal(a, actual, 0);
+            Assert.NotNull(response);
+            Assert.IsType<AccountModel>(response);
+            a.Should().BeEquivalentTo(response);
         }
+
+        /*[Fact]
+        public async Task AddAccount_ReturnsBadRequest_GivenInvalidModel()
+        {
+            // Arrange & Act
+            var mockRepo = new Mock<IAccountsRepository>();
+            var controller = new AccountsController(mockRepo.Object);
+            controller.ModelState.AddModelError("error", "some error");
+
+            // Act
+            var result = await controller.AccountCreate(AccountModel: null);
+
+            // Assert
+            Assert.IsType<BadRequestObjectResult>(result);
+        }*/
     }
 }
