dbb05b1f98ced783b8dee27b266388397715172e
Gabriel Daoust <gabrieldaoust1@hotmail.com>
2021-10-11 23:53:56 -0400

    Fixes des tests

--- REST/SmallTest/Controllers/AccountsControllerTests.cs
+++ REST/SmallTest/Controllers/AccountsControllerTests.cs
@@ -32,32 +32,38 @@ namespace REST.SmallTest.Controllers
         public async void GivenCreateAccount_WhenCreateAccountVaild_ThenResponseIsCorrectModel()
         {
             //Arrange
-            var sut = this.CreateAccountsController();
+            Mock<IAccountsRepository> mockAccountRepository = new();
+            mockAccountRepository.Setup(m => m.Create(accountA)).ReturnsAsync(accountA);
+            AccountsController sut = this.CreateAccountsController(mockAccountRepository.Object);
 
             //Act
             ActionResult<AccountModel> response = await sut.Create(accountA);
 
             //Assert
             Assert.IsType<CreatedAtActionResult>(response?.Result);
-
+            mockAccountRepository.Verify(v => v.Create(accountA));
         }
         [Fact]
         public async void GivenCreateAccount_WhenCreateAccountVaild_ThenResponseIsCorrespondingToGiven()
-        {          
+        {
             //Arrange
-            var sut = this.CreateAccountsController();
+            Mock<IAccountsRepository> mockAccountRepository = new();
+            mockAccountRepository.Setup(m => m.Create(accountA)).ReturnsAsync(accountA);
+            AccountsController sut = this.CreateAccountsController(mockAccountRepository.Object);
 
             //Act
             ActionResult<AccountModel> response = await sut.Create(accountA);
 
             //Assert
-            accountA.Should().BeEquivalentTo(response?.Value);
+            var createdAtActionResponse = (CreatedAtActionResult)response.Result;
+            accountA.Should().BeEquivalentTo((AccountModel)createdAtActionResponse?.Value);
+            mockAccountRepository.Verify(v => v.Create(accountA));
         }
         [Fact]
         public async void GivenNewAccountController_WhenAccountFindANonExistentAccountId_ThenResponseIsTypeNotFoundResult()
         {
             //Arrange
-            var sut = this.CreateAccountsController();
+            AccountsController sut = this.CreateAccountsController();
 
             //Act
             ActionResult<AccountModel> response = await sut.FindByAccountId(aNonExistentAccountId);
