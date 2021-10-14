bff58847d969562b7e240a243011388a2075dd3a
Gabriel Daoust <gabrieldaoust1@hotmail.com>
2021-10-13 23:13:59 -0400

    Fix pour compte null

--- Domain/Managers/TransactionsManager.cs
+++ Domain/Managers/TransactionsManager.cs
@@ -20,11 +20,12 @@ namespace Domain.Managers
                 AccountModel fromAccount = await _AccountsRepository.FindByAccountId(transaction.TransactionOrigin);
                 AccountModel toAccount = await _AccountsRepository.FindByAccountId(transaction.TransactionDestination);
 
-                if (fromAccount.AccountBalance >= transaction.TransactionAmount)
+                if (fromAccount != null && fromAccount.AccountBalance >= transaction.TransactionAmount)
                 {
-                    if (fromAccount != null) fromAccount.AccountBalance -= transaction.TransactionAmount;
-                    if (toAccount != null) toAccount.AccountBalance += transaction.TransactionAmount;
+                    fromAccount.AccountBalance -= transaction.TransactionAmount;
+                    if (toAccount != null) toAccount.AccountBalance += transaction.TransactionAmount; //Compte externe
                 }
+                else transaction.IsValid = false;
             }
             else transaction.IsValid = false;
             await _TransactionsRepository.Create(transaction);
