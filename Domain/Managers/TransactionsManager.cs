2de507dad358999b85521a63dd23bdda0adc767c
Gabriel Daoust <gabrieldaoust1@hotmail.com>
2021-10-14 12:06:32 -0400

    Revamp Transfer

--- Domain/Managers/TransactionsManager.cs
+++ Domain/Managers/TransactionsManager.cs
@@ -1,6 +1,7 @@
 ﻿using Domain.Interfaces;
 using Domain.Models;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Domain.Managers
@@ -16,24 +17,26 @@ namespace Domain.Managers
         }
         public async Task<TransactionModel> Transfer(TransactionModel transaction)
         {
-            /*List<int> ids = new();
-            ids.Add(transaction.TransactionOrigin);
-            ids.Add(transaction.TransactionDestination);
-            AccountModel fromAccount = await _AccountsRepository.FindByAccountId(transaction.TransactionOrigin); 
-            AccountModel toAccount = await _AccountsRepository.FindByAccountId(transaction.TransactionDestination);
-            if (transaction.TransactionAmount > 0
-                && fromAccount != null
-                && toAccount != null
-                && fromAccount.AccountBalance >= transaction.TransactionAmount)
+            transaction.IsValid = false;
+            if (transaction.TransactionAmount > 0)
             {
-                fromAccount.AccountBalance -= transaction.TransactionAmount;
-                toAccount.AccountBalance += transaction.TransactionAmount;
+                List<int> ids = new();
+                ids.Add(transaction.TransactionOrigin);
+                ids.Add(transaction.TransactionDestination);
+                List<AccountModel> accounts = await _AccountsRepository.FindByAccountsIds(ids);
+                AccountModel accountOrigin = accounts.Find(x => x.AccountId == transaction.TransactionOrigin);
+                AccountModel accountDestination = accounts.Find(x => x.AccountId == transaction.TransactionDestination);
+                if (accountOrigin != null &&
+                    accountDestination != null &&
+                    accountOrigin.AccountBalance >= transaction.TransactionAmount)
+                {
+                    accountOrigin.AccountBalance -= transaction.TransactionAmount;
+                    accountDestination.AccountBalance += transaction.TransactionAmount;
+                    transaction.IsValid = true;
+                }
             }
-            else transaction.IsValid = false;
             await _TransactionsRepository.Create(transaction);
-            return transaction;*/
-            await Task.Delay(1);
-            throw new NotImplementedException();
+            return transaction;
         }
     }
 }
